b051602b6c6ad6afd1f8363f65c98569fe282933
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-07-22 06:13:31 -0300

    Agregando json e intities Student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../shared/emtities';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ToolbarComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, ToolbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-entrega-1';
+  students: Student[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get<Student[]>('assets/mocks/students.json').subscribe(data => {
+      this.students = data;
+    });
+  }
 }
