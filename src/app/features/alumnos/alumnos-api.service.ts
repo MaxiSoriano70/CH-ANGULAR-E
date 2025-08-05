2496f3b30850a38efd29a998d080614ae046bf4b
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-08-05 03:17:05 -0300

    Generando un servidor para nuestra db.json

---
+++ src/app/features/alumnos/alumnos-api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../../../shared/emtities';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnosAPIService {
+  baseUrl = "http://localhost:3000";
+
+  constructor(private http: HttpClient) { }
+
+  getAlumnos(): Observable<Student[]>{
+    return this.http.get<Student[]>(`${this.baseUrl}/students`);
+  }
+}
