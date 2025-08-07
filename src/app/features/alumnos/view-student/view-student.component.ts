50472ca7612ecf42c636074563386fd37813cb9e
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-08-07 02:38:32 -0300

    Agregando vista a view-student y cambiando el css para que las sections qu estar dentro de router ocupen el 100% de ancho

--- src/app/features/alumnos/view-student/view-student.component.ts
+++ src/app/features/alumnos/view-student/view-student.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { Student } from '../../../../shared/emtities';
 import { Router } from '@angular/router';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, Location } from '@angular/common';
 
 @Component({
   selector: 'app-view-student',
-  imports: [JsonPipe],
+  imports: [],
   templateUrl: './view-student.component.html',
   styleUrl: './view-student.component.css'
 })
 export class ViewStudentComponent {
   student: Student | undefined;
 
-  constructor(private router: Router){
+  constructor(private router: Router, private location: Location){
     const navigation = this.router.getCurrentNavigation();
     this.student = navigation?.extras.state?.["student"];
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
