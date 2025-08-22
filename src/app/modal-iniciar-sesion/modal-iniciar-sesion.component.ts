37d5d6ef85f27bcb7296a235920dd01f32d9e948
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-08-21 23:38:40 -0300

    Arreglando logica del componente modal-iniciar-session

--- src/app/modal-iniciar-sesion/modal-iniciar-sesion.component.ts
+++ src/app/modal-iniciar-sesion/modal-iniciar-sesion.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-iniciar-sesion',
@@ -8,10 +9,18 @@ import { FormsModule, NgForm } from '@angular/forms';
   templateUrl: './modal-iniciar-sesion.component.html',
   styleUrl: './modal-iniciar-sesion.component.css'
 })
+
 export class ModalIniciarSesionComponent {
+  constructor(public activeModal: NgbActiveModal) {}
+
+  close() {
+    this.activeModal.dismiss();
+  }
+
   onSubmit(form: NgForm) {
     if (form.valid) {
-      console.log('Datos:', form.value);
+      console.log('Datos de login:', form.value);
+      this.activeModal.close(form.value); // devolvemos los datos al navbar
     } else {
       console.log('Formulario inválido');
     }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RoutePaths } from '../../shared/routes';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalIniciarSesionComponent } from "../modal-iniciar-sesion/modal-iniciar-sesion.component";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, ModalIniciarSesionComponent],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   routePaths = RoutePaths;
+
+  constructor(private modalService: NgbModal) {}
+
+  abrirModal() {
+    const modalRef = this.modalService.open(ModalIniciarSesionComponent, { centered: true });
+
+    modalRef.result.then(
+      (data) => {
+        if (data) {
+          console.log('Usuario inició sesión:', data);
+          // Acá podés guardar en un servicio, localStorage, etc.
+        }
+      },
+      () => {
+        console.log('Modal cerrado sin iniciar sesión');
+      }
+    );
+  }
 }
