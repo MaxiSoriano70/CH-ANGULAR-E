cb9b7ec984dc8356719d73264d488ad4efa3668b
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2025-08-24 23:21:59 -0300

    Agregando componente veterinaria

---
+++ src/app/veterinaria/veterinaria.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veterinaria',
+  imports: [],
+  templateUrl: './veterinaria.component.html',
+  styleUrl: './veterinaria.component.css'
+})
+export class VeterinariaComponent {
+
+}
